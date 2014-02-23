20dc3609981afead0590577a38dd0e7308f5910c
dsandor <david.sandor@gmail.com>
2014-02-23 14:07:30 -0600

    updated tag cloud code

--- js/dsTagCloud.js
+++ js/dsTagCloud.js
@@ -4,24 +4,84 @@
 
 $.widget("ds.tagcloud", {
 
-    options: { getdataurl: 'http://davidsandor.com/blogapi/api/Blog/GetAllCategories' },
+    options: {
+        getdataurl: 'http://davidsandor.com/blogapi/api/Blog/GetAllCategories',
+        categoryurltemplate: '?viewCategory='
+    },
 
     _create: function() {
         // build out the initial state / html of the widget
         this.element
-            .addClass("tagcloud")
-            .text("Bitch please: " + this.options.getdataurl);
+            .addClass("tagcloud");
+            //.text("Bitch please: " + this.options.getdataurl);
+        this._getData(this.element, this.options);
     },
 
     getdataurl: function(value) {
         if ( value === undefined )
         {
-            alert('ttt');
             return this.options.getdataurl;
         }
 
-        alert('new value:' + value);
         this.options.getdataurl = value;
-        this.element.text("Updated to: " + value);
+    },
+
+    categoryurltemplate: function(value) {
+        if ( value === undefined )
+        {
+            return this.options.categoryurltemplate;
+        }
+
+        this.options.categoryurltemplate = value;
+    },
+
+    _getData: function(e, opt) {
+        jQuery.support.cors = true;
+        $.ajax({
+            url: this.getdataurl(),
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                var newHtml = "<!-- start of tag cloud -->";
+
+                // Find highest frequency.
+                var maxFreq = 0;
+                $.each(data, function(index, category) {
+                   if (category.Frequency > maxFreq) maxFreq = category.Frequency;
+                });
+
+                $.each(data, function(index, category) {
+                    var intensityClassName = "tc-intensity-1";
+
+                    if (category.Frequency > 0)
+                    {
+                        var perBand = maxFreq / 5;
+
+                        // figure out what intensity this item is.
+                        var intensity = Math.floor(category.Frequency/perBand);
+
+                        if (intensity > 5) intensity = 5;
+                        if (intensity < 1) intensity = 1;
+
+                        intensityClassName = "tc-intensity-" + intensity;
+                    }
+
+                    newHtml += "<div class='" + intensityClassName + " tc-slight-pad'>"
+                        + "<a href='" + opt.categoryurltemplate + category.Id + "' class='tc-category-href'>"
+                        + category.Name + " ("
+                        + category.Frequency + ")"
+                        + "</a></div>";
+                });
+
+                newHtml += "<!-- end of tag cloud -->";
+
+                e.html(newHtml);
+
+                //alert(newHtml);
+            },
+            error: function (x, y, z) {
+                alert(x + '\n' + y + '\n' + z);
+            }
+        });
     }
 });
